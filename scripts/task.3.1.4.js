d3a14daf3e6a4c50f398922a3537a98c543d610c
Joni-Smith <72577555+Joni-Smith@users.noreply.github.com>
2020-11-09 13:13:22 +0000

    Update task.3.1.4.js
    practiced JS Arithemic Operators

---
+++ scripts/task.3.1.4.js
@@ -0,0 +1,16 @@
+var numberOfKittens = 5;
+
+var numberOfPuppies = 6;
+
+var numberOfAnimals = numberOfKittens + numberOfPuppies;
+
+var numberOfPets = numberOfKittens * numberOfPuppies;
+
+var totalCuteness = numberOfPuppies / numberOfKittens;
+
+console.log(numberOfAnimals);
+
+alert (numberOfPets);
+
+document.write(totalCuteness);
+
