ce116851ab0a5961bf8406408005ef091b35b9cb
Joni-Smith <72577555+Joni-Smith@users.noreply.github.com>
2020-11-10 21:06:59 +0000

    Update task.3.2.5.js

--- scripts/task.3.2.5.js
+++ scripts/task.3.2.5.js
@@ -1,6 +1,15 @@
-var temperature = 40;
+var temperature = 60;
 
-if( temperature < 50 ) {
+if( temperature < 0) {
 
-    console.log("Put on your coat");
+    console.log("Stay inside");
+
+} else if(temperature < 30) {
+    console.log("Wear a coat and a hat");
+
+} else if( temperature < 50){
+    console.log("Wear a coat");
+} else {
+    console.log("Just pants and vest");
+
 }
