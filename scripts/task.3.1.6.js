b7953e30bb7d976143d23537ecf14d0057ba9bdc
Joni-Smith <72577555+Joni-Smith@users.noreply.github.com>
2020-11-10 19:14:40 +0000

    Update task.3.1.6.js

--- scripts/task.3.1.6.js
+++ scripts/task.3.1.6.js
@@ -4,7 +4,7 @@ var tipPercentage = 12.5;
 
 var percentageOfPreTip = ( preTip * tipPercentage )/ 100;
 
-var TotalBill = "Your total bill, including tip is £" + preTipTotal + Tip;
+var TotalBill = "Your total bill, including tip is £" + preTip + percentageOfPreTip;
 
 document.write(TotalBill);
 
