d7f7cd0f0f32977da6e9c257253af86482d746e3
Joni-Smith <72577555+Joni-Smith@users.noreply.github.com>
2020-11-10 19:55:38 +0000

    Update task.3.2.2.js

---
+++ scripts/task.3.2.2.js
@@ -0,0 +1,9 @@
+function FullName ( firstName, lastName ){
+
+    console.log(firstName + ' ' + lastName );
+
+}
+
+FullName("Joni",  "Smith" );
+FullName("Ryan", "Peet");
+FullName("Jade", "Hodgkinson");
