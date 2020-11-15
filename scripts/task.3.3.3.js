3a2a0d30cc8b133b7485f04d705f447243605956
Joni-Smith <72577555+Joni-Smith@users.noreply.github.com>
2020-11-15 15:06:56 +0000

    Create task.3.3.3.js

---
+++ scripts/task.3.3.3.js
@@ -0,0 +1,19 @@
+function calculator( number1, number2, operator) {
+
+switch (operator ) {
+
+    case '+' :
+        console.log ( number1 + number2);
+        break;
+
+        case '-' :
+            console.log ( number1 - number2);
+            break;
+    
+    default :
+    // ERROR MESSAGE
+        break;
+}
+
+
+}
