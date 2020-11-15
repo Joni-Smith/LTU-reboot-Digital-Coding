f9d940f289639b4fb7114b58f496630c52d8a96e
Joni-Smith <72577555+Joni-Smith@users.noreply.github.com>
2020-11-15 13:47:30 +0000

    added switch function to with drink case

---
+++ scripts/task.3.3.2.js
@@ -0,0 +1,30 @@
+function drinkOrder( drink, size ) {
+
+    switch (drink) {
+        case "cola" :
+      console.log("You have ordered a "  + drink );
+            break;
+
+        case "lemonade" :
+            console.log("You have ordered a "  + drink );
+        break;
+
+        case "orangeade" :
+            console.log("You have ordered a "  + drink );
+        break;
+
+        default:
+            console.log("The drink that you have tried to order, does not exist");
+        break;
+    }
+
+
+
+
+
+
+
+
+
+
+}
