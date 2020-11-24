928829aec04e8b1520be4bc1381b18ca45160681
Joni-Smith <72577555+Joni-Smith@users.noreply.github.com>
2020-11-24 20:22:12 +0000

    Issue with JS script not linking
    this was down to a typo in how ive named the js task3.6.1.js not task.3.6.1.js

---
+++ scripts/task3.6.1.js
@@ -0,0 +1,3 @@
+var image1 = document.querySelector('img');
+
+image1.setAttribute( 'src', 'https://media.istockphoto.com/photos/cat-and-dog-playing-stock-photo-picture-id1180024506?k=6&m=1180024506&s=612x612&w=0&h=vi1g6h85yT1l12QVg0EC4BpmnlGckHRm4PqSwFhtpnE=');
