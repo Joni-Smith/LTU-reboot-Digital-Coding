1ac07e7276c38ad413a586e2f52aef42459e32eb
Joni-Smith <72577555+Joni-Smith@users.noreply.github.com>
2020-11-26 20:08:07 +0000

    added script in html and also in JS

---
+++ scripts/task.3.7.1.js
@@ -0,0 +1,14 @@
+const listItems = document.querySelectorAll("button.selection");
+
+const allImages = document.querySelectorAll(".main .image-selections .images");
+
+function toggleActiveClass( active ) {
+
+listItems.forEach( function( item ) {
+    item.classList.remove("active");
+
+})
+
+    active.classList.add("active");
+
+}
